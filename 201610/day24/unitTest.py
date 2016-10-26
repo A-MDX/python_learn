ea5928615243cff90a4d3d5bbab5476e0367dc06
A-mdx <634187415@qq.com>
2016-10-26 22:51:15 +0800

    准备学习单元测试2

--- 201610/day24/unitTest.py
+++ 201610/day24/unitTest.py
@@ -1,4 +1,19 @@
 __author__ = 'A-mdx'
 # -*- coding: utf-8 -*-
 
+class Dict(dict):
+	def __init__(self,**kw):
+		super().__init__(**kw)
+	
+	def __getattr__(self, item):
+		try:
+			return self[item]
+		except KeyError:
+			raise AttributeError(r"'dict' object has no attribute '%s'" % item)
+		
+	def __setattr__(self, key, value):
+		self[key] = value
+		
+# 为了编写单元测试，我们需要引入python自带的unittest模块。
+
 
